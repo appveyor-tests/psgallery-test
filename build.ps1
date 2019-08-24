8da13b9b5964e6a3d2f94ec89dddbc1eef5bd4d5
Alena Hrynkevich <alena@appveyor.com>
2019-08-24 11:57:30 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -6,15 +6,22 @@ echo 'Find-Module AppVeyorTestPsGallery'
 Find-Module AppVeyorTestPsGallery
 $ErrorActionPreference
 (Get-Content .\AppVeyorTestPsGallery\AppVeyorTestPsGallery.psd1).replace('<BUILD_VERSION>', $env:APPVEYOR_BUILD_VERSION) | Set-Content .\AppVeyorTestPsGallery\AppVeyorTestPsGallery.psd1
+echo mkdir
 mkdir $env:userprofile\Documents\WindowsPowerShell\Modules\AppVeyorTestPsGallery
+echo Copy-Item
 Copy-Item .\AppVeyorTestPsGallery\* $env:userprofile\Documents\WindowsPowerShell\Modules\AppVeyorTestPsGallery\ -Recurse -Force
+echo Publish-Module
 Publish-Module -Name AppVeyorTestPsGallery -NuGetApiKey $env:psg_api_key
+echo Remove-Item
 Remove-Item $env:userprofile\Documents\WindowsPowerShell\Modules\AppVeyorTestPsGallery -Recurse -Force
 Write-Host "Wait for 30 seconds until the module is published"; Start-Sleep -s 30
 echo 'Find-Module AppVeyorTestPsGallery'
 Find-Module AppVeyorTestPsGallery
 $ErrorActionPreference
+echo Install-Module
 Install-Module AppVeyorTestPsGallery -Scope AllUsers
 Get-PackageProvider
+echo Get-Module
 Get-Module
+$ErrorActionPreference
 dir $env:ProgramFiles\WindowsPowerShell\Modules\AppVeyorTestPsGallery\$env:APPVEYOR_BUILD_VERSION
